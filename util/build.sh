781da13412dd8990e44b9e525b1ebe2768aa8312
Alex Russell <slightlyoff@chromium.org>
2013-02-26 11:52:35 +0000

    Add a dependency-free version of the minified build and update the build.

--- util/build.sh
+++ util/build.sh
@@ -6,3 +6,7 @@
 # Run it through uglify.
 
 python post.py ../src/async-local-storage.js > ../bin/async-local-storage.min.js
+
+python post.py ../src/async-local-storage.js \
+               ../third_party/DOMFuture/polyfill/src/Future.js \
+                  > ../bin/async-local-storage-with-Future.min.js
